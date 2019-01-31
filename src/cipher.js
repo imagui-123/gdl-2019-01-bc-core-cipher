c3f54dba57cbc81ca53dd7fee323587ee1e4a3e0
Iris <iaguiarg@hotmail.com>
2019-01-31 10:12:49 -0600

    acomode la funcion decode

--- src/cipher.js
+++ src/cipher.js
@@ -6,20 +6,20 @@ let letter=[];
 let result="";
   for(let i=0; i<string.length; i++){
           letter[i]=string.charCodeAt(i)+ offset;
-            console.log(letter[i]);
+              console.log(letter[i]);
               // para saber si esta en mayusculas
         if(string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
                 letter[i]=((letter[i]-65) %26) + 65;
                 result +=String.fromCharCode(letter[i]);
-         } else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122) {
-                  letter[i]=((letter[i]-97) %26 ) + 97;
-                  result +=String.fromCharCode(letter[i]);
          }else if(string.charCodeAt(i)==32){
                   letter[i]=string.charCodeAt(i);
                   result += String.fromCharCode(letter[i]);
+         }else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122) {
+                  letter[i]=((letter[i]-97) %26 ) + 97;
+                  result +=String.fromCharCode(letter[i]);
          }
     }
-    alert(result);
+
     return result;
 
 },
@@ -34,7 +34,7 @@ let result="";
 
               // para saber si esta en mayusculas
         if(string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
-                letter[i]=((letter[i]-65) %26) + 65;
+                letter[i]=((letter[i]+65) %26) + 65;
                 result +=String.fromCharCode(letter[i]);
          } else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122) {
                   letter[i]=((letter[i]-97) %26 ) + 97;
@@ -44,7 +44,7 @@ let result="";
                   result += String.fromCharCode(letter[i]);
          }
     }
-    alert(result);
+
     return result;
 
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-// funciones para la pantalla de navegacion
-const openNav=()=> {
-  document.getElementById("mySidenav").style.width = "250px";
-};
-
-const closeNav=()=> {
-  document.getElementById("mySidenav").style.width = "0";
-};
-
 
 document.getElementById("cifrado").addEventListener("click",encode);
 
@@ -15,21 +6,20 @@ let string=document.getElementById('msj').value;
 let num=parseInt(document.getElementById('num').value);
 let msgCifrar=window.cipher.encode(num,string);//Mandamos llamar nuestro metodo cipher
 document.getElementById('resultado').innerHTML=msgCifrar; //Para visualizar el resultado en la pantalla
-};
+}
 
 
 document.getElementById("descifrado").addEventListener("click",decode);
 
 function decode(){
-  console.log("decodi");
 let string=document.getElementById('msj').value;
 let num=parseInt(document.getElementById('num').value)*-1;
 let msgDescifrar=window.cipher.decode(num,string);//Mandamos llamar nuestro metodo cipher
 document.getElementById('resultado').innerHTML=msgDescifrar; //Para visualizar el resultado en la pantalla
-};
+}
 
 function limpiar(){
  document.getElementById('msj').value="";
  document.getElementById('num').value="";
-};
+}
 document.getElementById("clean").addEventListener("click",limpiar);
