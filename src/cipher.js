ce016b97be95e487ac91ab668656c85a6db53b7f
Iris <iaguiarg@hotmail.com>
2019-01-31 20:52:50 -0600

    agregue la condicion para caracteres especiales

--- src/cipher.js
+++ src/cipher.js
@@ -5,21 +5,28 @@ encode (offset,string){
 let letter=[];
 let result="";
   for(let i=0; i<string.length; i++){
-          letter[i]=string.charCodeAt(i)+ offset;
-              console.log(letter[i]);
-              // para saber si esta en mayusculas
+          letter[i]=string.charCodeAt(i)+offset;
+
+                // para saber si esta en mayusculas
         if(string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
-                letter[i]=((letter[i]-65) %26) + 65;
+                letter[i]=((letter[i] - 65) %26) + 65;
                 result +=String.fromCharCode(letter[i]);
-         }else if(string.charCodeAt(i)==32){
+
+      }else if(string.charCodeAt(i)==32){
                   letter[i]=string.charCodeAt(i);
                   result += String.fromCharCode(letter[i]);
+
          }else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122) {
                   letter[i]=((letter[i]-97) %26 ) + 97;
                   result +=String.fromCharCode(letter[i]);
-         }
-    }
 
+         }else if(string.charCodeAt(i)<65 && string.charCodeAt(i)<90){
+                  letter[i]=((letter[i] - 33) %33) + 33;
+                  console.log(letter[i]);
+                  result +=String.fromCharCode(letter[i]);
+         }
+
+    }
     return result;
 
 },
@@ -33,19 +40,23 @@ let result="";
 
           letter[i]=string.charCodeAt(i);
 
-
               // para saber si esta en mayusculas
         if(string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
                 letter[i]=(((letter[i]+(offset*-1))+65) %26) + 65;
                 result +=String.fromCharCode(letter[i]);
 
          } else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122) {
-                  letter[i]=(((letter[i]+offset)+97) %26 ) + 97;
+                  letter[i]=((letter[i]+offset+97) %26 ) + 97;
                   console.log(letter[i]);
                   result +=String.fromCharCode(letter[i]);
+
          }else if(string.charCodeAt(i)==32){
                   letter[i]=string.charCodeAt(i);
                   result += String.fromCharCode(letter[i]);
+
+         }else if(string.charCodeAt(i)<65 && string.charCodeAt(i)<90){
+                  letter[i]=(((letter[i]-offset)+33) %33 ) + 33;
+                  result +=String.fromCharCode(letter[i]);
          }
     }
 
