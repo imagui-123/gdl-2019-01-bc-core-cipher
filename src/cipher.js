2563c5d121defbd4c6d398a1418b1f29d29b0895
Iris <iaguiarg@hotmail.com>
2019-02-06 12:24:27 -0600

    cambio de diseño en la pagina

--- src/cipher.js
+++ src/cipher.js
@@ -4,6 +4,7 @@ encode (offset,string){
 
 let letter=[];
 let result="";
+
   for(let i=0; i<string.length; i++){
           letter[i]=string.charCodeAt(i)+offset;
 
@@ -27,6 +28,7 @@ let result="";
          }
 
     }
+
     return result;
 
 },
@@ -55,19 +57,9 @@ let result="";
               result +=String.fromCharCode(letter[i]);
           }
 
-
-
-          }
-
-         //}
-         // else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122) {
-         //          letter[i]=(((letter[i]+offset)+97) %26 ) + 97;
-         //          result +=String.fromCharCode(letter[i]);
-         //
-         //}
-         else if(string.charCodeAt(i)==32){
-                  letter[i]=string.charCodeAt(i);
-                  result += String.fromCharCode(letter[i]);
+          }    else if(string.charCodeAt(i)==32){
+              letter[i]=string.charCodeAt(i);
+              result += String.fromCharCode(letter[i]);
 
          }else if(string.charCodeAt(i)<65 && string.charCodeAt(i)<90){
                   letter[i]=(((letter[i]-offset)+33) %33 ) + 33;
@@ -78,5 +70,4 @@ let result="";
     return result;
 
 }
-
 };
--- src/index.js
+++ src/index.js
@@ -1,34 +1,67 @@
 
-document.getElementById("cifrado").addEventListener("click",encode);
+document.getElementById("cifrado").addEventListener("click",comprobar);
+let Mensaje=document.getElementById('resultado');
+
+
 // let pResultado=document.getElementById('pantalla_resultado');
 
 function encode(){
 let string=document.getElementById('msj').value;
 let num=parseInt(document.getElementById('num').value);
 let msgCifrar=window.cipher.encode(num,string);//Mandamos llamar nuestro metodo cipher
-// resultado.style.display="flex";
-document.getElementById('resultado').innerHTML=msgCifrar; //Para visualizar el resultado en la pantalla
+Mensaje.style.display="flex";
+document.getElementById("input").reset();
+// console.log(msgCifrar);
+document.getElementById('resultaM').innerHTML=msgCifrar; //Para visualizar el resultado en la pantalla
 }
 
 
-document.getElementById("descifrado").addEventListener("click",decode);// Mandamos llamar al boton
+document.getElementById("descifrado").addEventListener("click", comprobarD);// Mandamos llamar al boton
 
 function decode(){
+  console.log("entra");
 let string=document.getElementById('msj').value;
-let num=parseInt(document.getElementById('num').value);
+let num=document.getElementById('num').value;
 let msgDescifrar=window.cipher.decode(num,string);//Mandamos llamar nuestro metodo cipher
-// pResultado.style.display="flex";
-document.getElementById('resultado').innerHTML=msgDescifrar; //Para visualizar el resultado en la pantalla
+Mensaje.style.display="flex";
+document.getElementById("input").reset();
+document.getElementById('resultaM').innerHTML=msgDescifrar; //Para visualizar el resultado en la pantalla
 }
 
-function limpiar(){
- document.getElementById('msj').value="";
- document.getElementById('num').value="";
- // pResultado.style.display="none";
-}
-
-// function resetForm(){
-//   document.getElementById('msj').reset();
-//   document.getElementById('num').reset();
-// }
 document.getElementById("clean").addEventListener("click",limpiar);
+function limpiar(){
+  location.reload(true);
+  // document.getElementById("input").reset();
+}
+
+document.getElementById('copy').addEventListener("click",copiar);
+function copiar(){
+  let copyText=document.getElementById('resultaM');
+  copyText.select();
+  document.execCommand("copy");
+}
+
+function comprobar() {
+let string=document.getElementById('msj').value;
+let num=document.getElementById('num').value;
+  if(string == "" || num==""){
+      alert("Necesitas llenar los campos");
+  }
+  else if (num <= 0 || num >= 100) {
+      alert("El número debe ser entre 1 y 99");
+  } else {
+    encode();
+  }
+}
+
+  function comprobarD() {
+  let string=document.getElementById('msj').value;
+  let num=parseInt(document.getElementById('num').value);
+    if(string == "" || num==""){
+        alert("Necesitas llenar los campos");
+    } else if (num <= 0 || num >= 100) {
+        alert("El número debe ser entre 1 y 99");
+    }  else {
+      decode();
+    }
+  }
