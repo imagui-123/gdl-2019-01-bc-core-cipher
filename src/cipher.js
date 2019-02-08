a812d8a620662c02e19c958fec49f2c3bb63218a
Iris <iaguiarg@hotmail.com>
2019-02-07 18:08:30 -0600

    mejora de las funciones

--- src/cipher.js
+++ src/cipher.js
@@ -1,72 +1,55 @@
 window.cipher = {
 
-encode (offset,string){
+  encode (offset,string){
 
-let letter=[];
-let result="";
+  let letter=[];
+  let result="";
+  let codigo="";
 
-  for(let i=0; i<string.length; i++){
-          letter[i]=string.charCodeAt(i)+offset;
-
-                // para saber si esta en mayusculas
-        if(string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
-                letter[i]=((letter[i] - 65) %26) + 65;
-                result +=String.fromCharCode(letter[i]);
-                // para saber si hay espacios
-      }else if(string.charCodeAt(i)==32){
-                  letter[i]=string.charCodeAt(i);
-                  result += String.fromCharCode(letter[i]);
-                  // para saber si son minusculas
-         }else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122) {
-                  letter[i]=((letter[i]-97) %26 ) + 97;
-                  result +=String.fromCharCode(letter[i]);
-                  // para caracteres especiales
-         }else if(string.charCodeAt(i)<65 && string.charCodeAt(i)<90){
-                  letter[i]=((letter[i] - 33) %33) + 33;
-                  console.log(letter[i]);
-                  result +=String.fromCharCode(letter[i]);
-         }
-
-    }
-
-    return result;
-
-},
-
-decode (offset,string){
-
-let letter=[];
-let result="";
-
-  for(let i=0; i<string.length; i++){
-
-          letter[i]=string.charCodeAt(i);
-
-              // para saber si esta en mayusculas
-        if(string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
-                letter[i]=(((letter[i]+(offset*-1))+65) %26) + 65;
-                result +=String.fromCharCode(letter[i]);
-
-         } else if(string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
-            if(offset>26){
-              letter[i]=(((letter[i]+offset)+97) %26 ) + 97;
-              result +=String.fromCharCode(letter[i]);
-          }  else {
-            letter[i]=letter[i]- offset;
-              result +=String.fromCharCode(letter[i]);
+    for(let i=0; i<string.length; i++){
+           codigo=string.charCodeAt(i);
+          console.log(codigo);
+          if(codigo>=97 && codigo<=122){
+            result=(((codigo - 97 )+ offset) %26 ) + 97;
+          } else if(codigo==32){
+            result=codigo;
+          }else if(codigo<65 && codigo<90){
+            result=(((codigo + offset)-33) %33 ) +33;
           }
+          else {
+            result=((codigo-65+offset)%26+65);
+          }
+            letter.push(String.fromCharCode(result));
+      }
+      let resultado=letter.join("");
+      return resultado;
+  },
 
-          }    else if(string.charCodeAt(i)==32){
-              letter[i]=string.charCodeAt(i);
-              result += String.fromCharCode(letter[i]);
+  
+  decode (offset,string){
 
-         }else if(string.charCodeAt(i)<65 && string.charCodeAt(i)<90){
-                  letter[i]=(((letter[i]-offset)+33) %33 ) + 33;
-                  result +=String.fromCharCode(letter[i]);
-         }
-    }
+  let letter=[];
+  let result="";
+  let codigo="";
 
-    return result;
+    for(let i=0; i<string.length; i++){
 
-}
+            codigo=string.charCodeAt(i);
+            console.log(codigo);
+            if(codigo>=97 && codigo<=122){
+              result=(((codigo - 122)+ ( - offset))%26) +122;
+            }else if(codigo==32){
+              result=codigo;
+            }else if(codigo<65 && codigo <90){
+              result=(((codigo - offset) +33) %33) +33;
+              console.log(result);
+            }else{
+              result=((codigo + 65 + (offset* -1)) % 26) + 65;
+            }
+              letter.push(String.fromCharCode(result));
+          }
+          let resultado=letter.join("");
+          return resultado;
+        }
+
 };
